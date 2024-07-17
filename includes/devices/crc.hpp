172bee95b0fa153db1de6dc088ae267a8639bdf6
ulastanersoyak <ulassztanersoyak@gmail.com>
2024-07-17 03:52:38 +0300

    crc bug

--- includes/devices/crc.hpp
+++ includes/devices/crc.hpp
@@ -4,29 +4,79 @@
 #include <array>
 #include <cstdint>
 
+// template <std::uint8_t data_width>
+// [[nodiscard]] constexpr std::uint16_t
+// crc16_hash (const std::array<std::uint8_t, data_width> &bit_array) noexcept
+// {
+//   std::uint16_t crc = 0xFFFF;
+//   constexpr std::uint16_t polynomial = 0xA001;
+//
+//   for (std::uint8_t byte : bit_array)
+//     {
+//       crc ^= byte;
+//
+//       for (std::size_t j = 0; j < bit_array.size (); ++j)
+//         {
+//           if (crc & 0x0001U)
+//             {
+//               crc = static_cast<std::uint16_t> ((crc >> 1U)) ^ polynomial;
+//             }
+//           else
+//             {
+//               crc >>= 1U;
+//             }
+//         }
+//     }
+//   return crc;
+// }
+
 template <std::uint8_t data_width>
 [[nodiscard]] constexpr std::uint16_t
 crc16_hash (const std::array<std::uint8_t, data_width> &bit_array) noexcept
 {
-  std::uint16_t crc = 0xFFFF;
-  constexpr std::uint16_t polynomial = 0xA001;
+  constexpr std::uint16_t CRC16 = 0x8005;
+  std::uint16_t out = 0;
+  int bits_read = 0;
+  int bit_flag = 0;
 
-  for (std::uint8_t byte : bit_array)
+  for (const auto &data : bit_array)
     {
-      crc ^= byte;
+      bit_flag = out >> 15;
 
-      for (std::size_t j = 0; j < bit_array.size (); ++j)
+      out <<= 1;
+      out |= (data >> bits_read) & 1;
+
+      bits_read++;
+      if (bits_read > 7)
         {
-          if (crc & 0x0001U)
-            {
-              crc = static_cast<std::uint16_t> ((crc >> 1U)) ^ polynomial;
-            }
-          else
-            {
-              crc >>= 1U;
-            }
+          bits_read = 0;
+        }
+
+      if (bit_flag)
+        {
+          out ^= CRC16;
         }
     }
+
+  for (int i = 0; i < 16; ++i)
+    {
+      bit_flag = out >> 15;
+      out <<= 1;
+      if (bit_flag)
+        {
+          out ^= CRC16;
+        }
+    }
+
+  std::uint16_t crc = 0;
+  for (std::uint16_t i = 0x8000, j = 0x0001; i != 0; i >>= 1, j <<= 1)
+    {
+      if (i & out)
+        {
+          crc |= j;
+        }
+    }
+
   return crc;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "interfaces/interface.hpp"
 
 #if (TEST == 1)
-#include "crc_test/crc_test.hpp"
 #include "device_test/master_test.hpp"
 #include "device_test/slave_test.hpp"
 #endif // ! TEST
@@ -13,10 +12,13 @@ main ()
   master_bus_test::bus_byte_size ();
   slave_bus_tests::bus_byte_size ();
   slave_bus_tests::bus_bit_size ();
-  crc_test::hash_test ();
 #endif // !TEST
   interface<16> intr;
-  intr.get_package_from_slave ();
-  intr.send_package_to_master ();
+  for (int i = 0; i < 10; ++i)
+    {
+      intr.get_package_from_slave ();
+      intr.send_package_to_master ();
+      std::println ("\n");
+    }
   return 0;
 }
